ee7f25beb55c6ccd563008bf7d508b68837a0b9f
Frederik Creemers <frederikcreemers@gmail.com>
2016-07-25 19:30:50 +0200

    Clicking an icon makes a choice in the RockPaperScissors phase.

--- src/game/RockPaperScissors.js
+++ src/game/RockPaperScissors.js
@@ -5,12 +5,21 @@ import paperSrc from '../icons/paper.png';
 import scissorsSrc from '../icons/scissors.png';
 
 export default (props) => {
-    const imgStyle={
-        maxWidth: "25%",
-        borderRadius: "50px"
-    };
-    return (
-        <div style={{backgroundColor: "white", color: "#333", padding: "16px"}}>
+    var content;
+    if (!props.gameData.iPlayedRockPaperScissors){
+        const imgStyle = {
+            maxWidth: "25%",
+            borderRadius: "10%",
+            cursor: "pointer"
+        };
+        const playRock = props.manager.playRockPaperScissors.bind(
+            props.manager, props.gameData, "rock");
+        const playPaper = props.manager.playRockPaperScissors.bind(
+            props.manager, props.gameData, "paper");
+        const playScissors = props.manager.playRockPaperScissors.bind(
+            props.manager, props.gameData, "scissors");
+
+        content = <div>
             <h2>Rock Paper Scissors</h2>
             <p>
                 To decide who gets to pick their role (order or chaos), we play
@@ -22,10 +31,21 @@ export default (props) => {
                 Click or tap one of these images.
             </p>
             <div style={{padding: "0.2em", textAlign: "center"}}>
-                <img className="rpcImage" src={rockSrc} style={imgStyle} />
-                <img className="rpcImage" src={paperSrc} style={imgStyle} />
-                <img className="rpcImage" src={scissorsSrc} style={imgStyle} />
+                <img className="rpcImage" src={rockSrc} style={imgStyle}
+                     onClick={playRock}/>
+                <img className="rpcImage" src={paperSrc} style={imgStyle}
+                     onClick={playPaper}/>
+                <img className="rpcImage" src={scissorsSrc} style={imgStyle}
+                     onClick={playScissors}/>
             </div>
+        </div>
+    } else {
+        content = <p>Waiting for your opponent to make their move...</p>
+    }
+
+    return (
+        <div style={{backgroundColor: "white", color: "#333", padding: "16px"}}>
+            {content}
         </div>
     );
 }
