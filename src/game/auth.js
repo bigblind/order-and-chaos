515276ea78e5f75f41c929c4d83a00f81bba20a0
Frederik Creemers <frederikcreemers@gmail.com>
2016-07-25 19:27:32 +0200

    Fixed an issue where the user would get a new ID every time they reload the page.

--- src/game/auth.js
+++ src/game/auth.js
@@ -1,3 +1,14 @@
 import firebase from "firebase";
 
-export const user = firebase.auth().signInAnonymously();
+export const user = new Promise((resolve) => {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            resolve(user);
+        } else {
+            firebase.auth().signInAnonymously();
+        }
+    });
+}).then((user) => {
+    console.log("userId: "+user.uid);
+    return user;
+});
